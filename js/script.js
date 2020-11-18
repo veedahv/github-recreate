19f99f9e1e29a7baa2cc595cf2059295835b2f3d
veedahv <veedahv55@gmail.com>
2020-11-18 22:37:45 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,14 @@
 
 
-
-// https://api.github.com/graphql
-
-// const repoUrl = curl -H "Authorization: bearer token" -X POST -d " \
-//  { \
-//    \"query\": \"query { viewer { login }}\" \
-//  } \
-// " https://api.github.com/graphql
-// 7e111cdbc98967510634840bfe18fa0a357f2aab
-// const repoUrl = "Authorization: bearer 7e111cdbc98967510634840bfe18fa0a357f2aab" -X POST -d " \
-//  { \
-//    \"query\": \"query { viewer { login }}\" \
-//  } \
-// " https://api.github.com/graphql
 const repoUl = document.querySelector('.repo-ul');
 const userImgs = document.querySelectorAll('.userImg');
 const userName = document.querySelector('.user-name');
-const userLogin = document.querySelector('.user-login');
+const repoCount = document.querySelector('.repo-count');
+const userLogins = document.querySelectorAll('.user-login');
 const userBio = document.querySelector('.user-bio');
-const userUrlApi = `https://api.github.com/users/veedahv`;
 const userUrlQl = `https://api.github.com/graphql`;
 
-// fetch(userUrlApi).then( function (response) {
-//   console.log(response);  
-//   return response.json()
-// }).then( function (data) {
-//   console.log(data);  
-//   console.log(data.bio); 
-//   document.querySelector('header').innerHTML = data.bio
-// })
+
 function userInfoAll(infoTag, repoTag) {
   console.log(infoTag);
   console.log(infoTag.login);
@@ -38,16 +17,22 @@ function userInfoAll(infoTag, repoTag) {
   console.log(repoTag.nodes[2].name);
   console.log('working');
   let repoNodes = repoTag.nodes.reverse();
-  userLogin.textContent = infoTag.login;
   userName.textContent = infoTag.name;
+  userLogins.forEach(userLogin => {    
+    userLogin.textContent = infoTag.login;
+  });
   userBio.textContent = infoTag.bio;
   console.log(repoNodes);
   userImgs.forEach(userImg => {
     userImg.src = infoTag.avatarUrl;
   })
+  let totalRepoCount = repoTag.totalCount;
+  console.log(totalRepoCount)
+  repoCount.textContent = repoTag.totalCount;
   repoNodes.forEach(repoNode => {
     console.log(repoNode.name);  
     let repoName = repoNode.name;
+    let repoDesc = repoNode.description;
     let repoUpdate = repoNode.updatedAt;
     let repoDate = new Date(repoUpdate);
     let repoLang = repoNode.languages.nodes[0].name;
@@ -56,36 +41,48 @@ function userInfoAll(infoTag, repoTag) {
     console.log(repoDate);
     console.log(repoDate.toDateString());
     console.log(repoDate.getMinutes());
+    console.log(repoDate.getMonth());
+    console.log(repoDate.getDate());
 
     const repoLi = `
     <li class="flex">
     <div class="repo-info">
         <h2 class="repo-name"><a href="">${repoName}</a></h2>
-        <div class="flx">
-            <span class="lang-contain">
+        <p class="repo-desc">${repoDesc}</p>
+        <div class="flex">
+            <span class="lang-contain flex">
                 <span class="lang-bg" style="background-color: ${repoBg};"></span>
                 <span class="lang-name">${repoLang}</</span>
             </span>
             <span class="update">
-                Updated <span class="time"></span> ago
+                Updated <span class="time"></span>
             </span>
         </div>
     </div>
+    <button class="star"><i class="fa fa-star-o"></i> Star</button>
 </li>
     `;
 
     repoUl.innerHTML += repoLi;  
   });
+  let repoDescTxts = repoUl.querySelectorAll('.repo-desc');
+  repoDescTxts.forEach(repoDescTxt => {
+    console.log(repoDescTxt.innerHTML);    
+    if (repoDescTxt.innerHTML = 'null' ) {
+      repoDescTxt.style.display = 'none'
+    } else {
+      repoDescTxt.style.display = 'inline-block'
+      
+    }
+  });
 
 }
-// repositoryOwner (login: "veedahv") {
-  
-  // forksCount
+
 fetch(userUrlQl, {
   method: 'POST',
   headers: {
     "Content-Type": "application/json",
-    "Authorization": "bearer 7e111cdbc98967510634840bfe18fa0a357f2aab",
+    "Authorization": "bearer ",
   },
   body: JSON.stringify({
     query: `
@@ -96,8 +93,10 @@ fetch(userUrlQl, {
             name
             bio
             repositories(last: 20) {
+              totalCount
               nodes {
                 name
+                description
                 forkCount
                 stargazerCount
                 updatedAt
@@ -136,5 +135,43 @@ fetch(userUrlQl, {
   // errorMsg(error);
 })
 
+const isInViewport = (element, el) => {
+  const rect = element.getBoundingClientRect();
+  if (
+      rect.bottom >= 0 &&
+      rect.left >= 0 &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  ) {
+    console.log('ahh')
+    el.classList.remove('fixed')
+  } else {
+    console.log('nahh')
+    el.classList.add('fixed')
+  }
+}
 
+const isImgViewport = (element, el) => {
+  const rect = element.getBoundingClientRect();
+  if (
+      rect.bottom >= 0 &&
+      rect.left >= 0 &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  ) {
+    console.log('ahh')
+    el.style.visibility = 'hidden'
+  } else {
+    el.style.visibility = 'visible'
+    console.log('nahh')
+  }
+}
 
+document.addEventListener('scroll', (event) => {
+  let headerTag = document.querySelector('header');
+  let userTag = document.querySelector('.user-header');
+  let navFixed = document.querySelector('.mini-nav-desktop');
+  let navImgFixed = document.querySelector('.img-contain');
+  isInViewport(headerTag, navFixed);
+  isImgViewport(navImgFixed, userTag);
+})
